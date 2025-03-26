bbcba7cfb748dac9ca8c4af39144567b990e47c7
Pierre RASOLONJATOVO <tovo.aps.p2@gmail.com>
2025-03-26 08:55:02 +0300

    Add of DataFixtures and Repository directories

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Annonce;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory ::create("FR-fr");
+
+        for ($i=0; $i < 50; $i++) { 
+            $annonce = new Annonce();
+            $tittle = $faker->sentence();
+            $converImage = $faker->imageUrl(1000, 350);
+            $introduction = $faker->paragraph(3);
+            $content = '<p>'.join('</p><p>', $faker->paragraphs(5)).'</p>';
+            $annonce->setTittle($tittle)
+                    ->setPrice(mt_rand(30, 120))
+                    ->setIntroduction($introduction)
+                    ->setContent($content)
+                    ->setConverImage($converImage)
+                    ->setRooms(mt_rand(1, 8));
+            $manager->persist($annonce);
+        }
+        $manager->flush();
+    }
+}
