fcbf44d343acfe588e5cc7b7039c4794aaf04b03
Pierre RASOLONJATOVO <tovo.aps.p2@gmail.com>
2025-04-08 20:29:50 +0300

    Implemntation de la page insertion et initiation de l'utilisation de formulaire

---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('tittle')
+            ->add('slug')
+            ->add('price')
+            ->add('introduction')
+            ->add('content')
+            ->add('converImage')
+            ->add('rooms')
+            ->add('save', SubmitType::class, [
+                'label' => 'Creer le formulaire',
+                'attr' => [
+                    'class' => 'btn btn-secondary'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+        ]);
+    }
+}
